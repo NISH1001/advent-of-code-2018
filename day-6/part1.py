ae058bbdae9aef0b98ddf72af195e15be75a81fd
NISH1001 <nishanpantha@gmail.com>
2018-12-08 10:31:42 +0545

    Add partial solution for part 1 of day 6

--- day-6/part1.py
+++ day-6/part1.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+    This is a partial solution because it gives multiple answers and
+    I have to copy paste the answers one by one to find real one.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import manhattan_distances
@@ -13,43 +18,27 @@ def voronoi(coords, width=800, height=800, use_max=False):
         w, h = max(X) + 2, max(Y) + 2
     else:
         w, h = width, height
+    print("Computing voronoi diagram of size :: {}, {}".format(w, h))
 
+    minx, miny = min(X), min(Y)
     xx = list(range(w))
     yy = list(range(h))
     mat = np.array(list(itertools.product(xx, yy)))
 
     distances = manhattan_distances(mat, coord_matrix)
-    dist_max_idx = np.argmin(distances, axis=1)
+    dist_min_idx = np.argmin(distances, axis=1)
+    dist_min = np.min(distances, axis=1)
 
     arr = np.zeros((w, h))
     for i, p in enumerate(mat):
         r, c = p
-        arr[r, c] = dist_max_idx[i]
-    unique = np.unique(arr, return_counts=True)
-    print(unique)
-
-    # plt.plot(xx, yy, marker='.', color='k', linestyle='none')
-    plt.imshow(arr)
-    # plt.savefig('voronoi.jpg')
-    plt.show()
-
-def part2(coords):
-    coords = np.array(coords)
-    xvalues = np.arange(coords[:,0].max())
-    yvalues = np.arange(coords[:,1].max())
-    xx, yy = np.meshgrid(xvalues, yvalues)
-
-    layers = []
-    for coord in coords:
-        mdists = np.abs(xx - coord[0]) + np.abs(yy - coord[1])
-        layers.append(mdists)
-
-    dist_arr = np.array(layers)
-    print(dist_arr)
-    tot_dists = dist_arr.sum(axis=0)
-    print(tot_dists)
-    print((tot_dists < 10000).sum())
-
+        idx = dist_min_idx[i]
+        dist = distances[i]
+        mn = np.min(dist)
+        count = (dist == mn).sum()
+        val = dist_min_idx[i] if count == 1 else -1
+        arr[r, c] = val
+    return arr
 
 def main():
     coords = []
@@ -57,9 +46,22 @@ def main():
         for line in f:
             x, y = line.strip().split(',')
             coords.append((int(x), int(y)))
-    voronoi(coords, 800, 800)
+
+    v1 = voronoi(coords, 400, 400)
+    unique1 = np.unique(v1, return_counts=True)
+    print(unique1)
+    plt.imshow(v1)
+    plt.show()
+
+    v2 = voronoi(coords, 500, 500)
+    unique2 = np.unique(v2, return_counts=True)
+    print(unique2)
+    plt.imshow(v2)
+    plt.show()
+
+    closed = set(unique1[1]).intersection(set(unique2[1]))
+    print(sorted(closed, reverse=True))
 
 
 if __name__ == "__main__":
     main()
-
